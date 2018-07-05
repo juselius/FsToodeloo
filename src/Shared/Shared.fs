27242ebae7f5a175ebbf2a9dca04e53b05ad5af5
Jonas Juselius <jonas.juselius@itpartner.no>
2018-07-05 21:08:12 +0200

    Change Counter to Todo

--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -1,6 +1,13 @@
 namespace Shared
 
-type Counter = int
+open System
+
+type Todo = 
+    { taskId : int
+      priority : int
+      task : string
+      due : DateTime
+    }
 
 module Route =
     /// Defines how routes are generated on server and mapped from client
@@ -10,5 +17,6 @@ module Route =
 /// A type that specifies the communication protocol for client and server
 /// Every record field must have the type : 'a -> Async<'b> where 'a can also be `unit`
 /// Add more such fields, implement them on the server and they be directly available on client
-type ICounterProtocol =
-    { getInitCounter : unit -> Async<Counter> }
+type ITodoProtocol = 
+    { getInitTodo : unit -> Async<list<Todo>> 
+    }
