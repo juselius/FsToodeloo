4d9f4d47f551acad90f49a8c40d9e14ac2b628f3
Jonas Juselius <jonas.juselius@itpartner.no>
2018-08-23 19:52:55 +0200

    Update server to latest saturn and fable.remoting

--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -1,3 +1,5 @@
+module Toodeloo.Main
+
 open System.IO
 open System.Threading.Tasks
 
@@ -13,30 +15,30 @@ open Fable.Remoting.Giraffe
 let publicPath = Path.GetFullPath "../Client/public"
 let port = 8085us
 
-let getInitTodo () : Task<list<Todo>> = 
-    task { return [ 
-        { taskId = 1
-          priority = 1
-          task = "From Server with love."
-          due = System.DateTime.Now
-        } ]
+let getInitTodo () : Task<list<Todo>> =
+    task {
+        return [
+            { taskId = 1
+              priority = 1
+              task = "From Server with love."
+              due = System.DateTime.Now
+            } ]
     }
+let todoApi = {
+    initialTodo = getInitTodo >> Async.AwaitTask
+}
 
 let webApp =
-    let server =
-        { getInitTodo = getInitTodo >> Async.AwaitTask }
-    remoting server {
-        use_route_builder Route.builder
-    }
+    Remoting.createApi()
+    |> Remoting.withRouteBuilder Route.builder
+    |> Remoting.fromValue todoApi
+    |> Remoting.buildHttpHandler
 
 let app = application {
     url ("http://0.0.0.0:" + port.ToString() + "/")
-    router webApp
+    use_router webApp
     memory_cache
     use_static publicPath
-    // sitemap diagnostic data cannot be inferred when using Fable.Remoting
-    // Saturn issue at https://github.com/SaturnFramework/Saturn/issues/64
-    disable_diagnostics
     use_gzip
 }
 
--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -27,6 +27,6 @@ module Route =
 /// A type that specifies the communication protocol for client and server
 /// Every record field must have the type : 'a -> Async<'b> where 'a can also be `unit`
 /// Add more such fields, implement them on the server and they be directly available on client
-type ITodoProtocol = 
-    { getInitTodo : unit -> Async<list<Todo>> 
+type ITodoProtocol = { 
+    initialTodo : unit -> Async<list<Todo>> 
     }
